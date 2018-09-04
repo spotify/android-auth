66c1135d617b686d7c8df274e6014b73c348e6da
Jan <stoltmanjan@gmail.com>
2018-09-04 23:20:06 +0200

    Migration of MainActivity to Kotlin

---
+++ auth-sample-kotlin/src/main/java/com/spotify/sdk/android/authentication/sample/kotlin/MainActivity.kt
@@ -0,0 +1,159 @@
+/*
+ * Copyright (c) 2015-2016 Spotify AB
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.sdk.android.authentication.sample.kotlin
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.spotify.sdk.android.authentication.AuthenticationClient
+import com.spotify.sdk.android.authentication.AuthenticationRequest
+import com.spotify.sdk.android.authentication.AuthenticationResponse
+import com.spotify.sdk.android.authentication.BuildConfig.VERSION_NAME
+import okhttp3.*
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        const val CLIENT_ID = "089d841ccc194c10a77afad9e1c11d54"
+        const val AUTH_TOKEN_REQUEST_CODE = 0x10
+        const val AUTH_CODE_REQUEST_CODE = 0x11
+    }
+
+    private val mOkHttpClient = OkHttpClient()
+    private var mAccessToken: String? = null
+    private var mAccessCode: String? = null
+    private var mCall: Call? = null
+
+    private val redirectUri: Uri = Uri.Builder()
+            .scheme(getString(R.string.com_spotify_sdk_redirect_scheme))
+            .authority(getString(R.string.com_spotify_sdk_redirect_host))
+            .build()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.title = String.format(Locale.US, "Spotify Auth Sample %s", VERSION_NAME)
+    }
+
+    override fun onDestroy() {
+        cancelCall()
+        super.onDestroy()
+    }
+
+    fun onGetUserProfileClicked(view: View) {
+        if (mAccessToken == null) {
+            showRequestAccessTokenSnackbar()
+        } else {
+            requestUserProfile()
+        }
+    }
+
+    private fun requestUserProfile() {
+        val request = Request.Builder()
+                .url("https://api.spotify.com/v1/me")
+                .addHeader("Authorization", "Bearer $mAccessToken")
+                .build()
+
+        cancelCall()
+
+        mCall = mOkHttpClient.newCall(request)
+
+        mCall?.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                setResponse("Failed to fetch data: $e")
+            }
+
+            @Throws(IOException::class)
+            override fun onResponse(call: Call, response: Response) {
+                try {
+                    val jsonObject = JSONObject(response.body()!!.string())
+                    setResponse(jsonObject.toString(3))
+                } catch (e: JSONException) {
+                    setResponse("Failed to parse data: $e")
+                }
+            }
+        })
+    }
+
+    private fun showRequestAccessTokenSnackbar() {
+        val snackbar = Snackbar.make(findViewById(R.id.activity_main), R.string.warning_need_token, Snackbar.LENGTH_SHORT)
+        snackbar.view.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent))
+        snackbar.show()
+    }
+
+    fun onRequestCodeClicked(view: View) {
+        val request = getAuthenticationRequest(AuthenticationResponse.Type.CODE)
+        AuthenticationClient.openLoginActivity(this, AUTH_CODE_REQUEST_CODE, request)
+    }
+
+    fun onRequestTokenClicked(view: View) {
+        val request = getAuthenticationRequest(AuthenticationResponse.Type.TOKEN)
+        AuthenticationClient.openLoginActivity(this, AUTH_TOKEN_REQUEST_CODE, request)
+    }
+
+    private fun getAuthenticationRequest(type: AuthenticationResponse.Type): AuthenticationRequest {
+        return AuthenticationRequest.Builder(CLIENT_ID, type, redirectUri.toString())
+                .setShowDialog(false)
+                .setScopes(arrayOf("user-read-email"))
+                .setCampaign("your-campaign-token")
+                .build()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val response = AuthenticationClient.getResponse(resultCode, data)
+
+        if (AUTH_TOKEN_REQUEST_CODE == requestCode) {
+            mAccessToken = response.accessToken
+            updateTokenView()
+        } else if (AUTH_CODE_REQUEST_CODE == requestCode) {
+            mAccessCode = response.code
+            updateCodeView()
+        }
+    }
+
+    private fun setResponse(text: String) {
+        runOnUiThread {
+            findViewById<TextView>(R.id.response_text_view)?.text = text
+        }
+    }
+
+    private fun updateTokenView() {
+        findViewById<TextView>(R.id.token_text_view)?.text = getString(R.string.token, mAccessToken)
+    }
+
+    private fun updateCodeView() {
+        findViewById<TextView>(R.id.code_text_view)?.text = getString(R.string.code, mAccessCode)
+    }
+
+    private fun cancelCall() {
+        mCall?.cancel()
+    }
+}
