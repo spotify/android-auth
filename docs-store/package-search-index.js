175831163a0bd2cdb8bd0089d8b1c772e82f83da
Veronika Figura <veronikap@spotify.com>
2023-12-20 15:43:16 +0100

    Update docs

--- docs-store/package-search-index.js
+++ docs-store/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.spotify.sdk.android.auth"},{"l":"com.spotify.sdk.android.auth.app"},{"l":"com.spotify.sdk.android.auth.store"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.spotify.sdk.android.auth"},{"l":"com.spotify.sdk.android.auth.app"},{"l":"com.spotify.sdk.android.auth.store"}];updateSearchResults();
